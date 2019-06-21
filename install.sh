d574b5fca6ba378c26d080fd3ec4be813bc6e136
dmckminard <40671949+dmckminard@users.noreply.github.com>
2019-06-21 17:15:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,5 @@ sudo apt upgrade -y
 
 echo "Installing components"
 sudo ./install-bluetooth.sh
-sudo ./install-shairport.sh
-sudo ./install-spotify.sh
-sudo ./install-upnp.sh
+sudo ./install-snapcast-client.sh
 sudo ./install-startup-sound.sh
-sudo ./install-pivumeter.sh
-#sudo ./enable-hifiberry.sh
-sudo ./install-aplay-zero.sh
-sudo ./enable-read-only.sh
