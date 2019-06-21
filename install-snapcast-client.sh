145b7fd73c77d7d7197fa584c2569ffa6c31a385
dmckminard <40671949+dmckminard@users.noreply.github.com>
2019-06-21 17:28:46 +0200

    Update install-snapcast-client.sh

--- install-snapcast-client.sh
+++ install-snapcast-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-
+# Installation Snapcast et Snapserver
 SNAPCLIENT_VERSION=0.15.0
 SNAPSERVER_VERSION=0.15.0
 
@@ -18,3 +18,45 @@ rm snapclient_${SNAPCLIENT_VERSION}_armhf.deb
 rm snapserver_${SNAPSERVER_VERSION}_armhf.deb
 
 apt -f install -y
+
+# Config fichiers audio
+
+cat <<'EOF' > /etc/bluetooth/main.conf
+pcm.hifiberry {
+    type softvol
+    slave.pcm "plughw:0"
+    control.name "Master"
+    control.card 0
+}
+pcm.!default {
+    type             plug
+    slave.pcm       "hifiberry"
+}
+EOF
+
+sudo mkdir /home/btspeaker
+
+cat <<'EOF' > /etc/bluetooth/main.conf
+pcm.!default {
+        type plug
+        slave.pcm rate48000Hz
+}
+
+pcm.rate48000Hz {
+        type rate
+        slave {
+                pcm writeFile
+                format S16_LE
+                rate 48000
+        }
+}
+
+pcm.writeFile {
+        type file
+        slave.pcm null
+        file "/tmp/bluesnapfifo"
+        format "raw"
+}
+EOF
+
+
