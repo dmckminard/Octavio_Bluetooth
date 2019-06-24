b133fb095a112899597bf9046fc203bb0a24da88
dmckminard <40671949+dmckminard@users.noreply.github.com>
2019-06-24 10:24:25 +0200

    Update install_config_sound.sh

--- install_config_sound.sh
+++ install_config_sound.sh
@@ -3,21 +3,10 @@
 SNAPCLIENT_VERSION=0.15.0
 SNAPSERVER_VERSION=0.15.0
 
-echo -n "Installer Snapclient et Snapserver (snapclient v${SNAPCLIENT_VERSION}) (snapserver v${SNAPSERVER_VERSION})? [y/N] "
+echo -n "Snapcast deja install? (snapclient v${SNAPCLIENT_VERSION}) (snapserver v${SNAPSERVER_VERSION})? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 1; fi
 
-wget https://github.com/badaix/snapcast/releases/download/v${SNAPCLIENT_VERSION}/snapclient_${SNAPCLIENT_VERSION}_armhf.deb
-sudo dpkg -i snapclient_${SNAPCLIENT_VERSION}_armhf.deb
-
-
-wget https://github.com/badaix/snapcast/releases/download/v${SNAPSERVER_VERSION}/snapsever_${SNAPSERVER_VERSION}_armhf.deb
-sudo dpkg -i snapserver_${SNAPSERVER_VERSION}_armhf.deb
-
-rm snapclient_${SNAPCLIENT_VERSION}_armhf.deb
-rm snapserver_${SNAPSERVER_VERSION}_armhf.deb
-
-apt -f install -y
 
 # Config fichiers audio
 
