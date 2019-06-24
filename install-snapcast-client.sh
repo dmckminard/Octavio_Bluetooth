84d0a4b934c19f7437e1f83f4b235ac915874f57
dmckminard <40671949+dmckminard@users.noreply.github.com>
2019-06-24 10:14:00 +0200

    Update install-snapcast-client.sh

--- install-snapcast-client.sh
+++ install-snapcast-client.sh
@@ -8,11 +8,11 @@ read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 1; fi
 
 wget https://github.com/badaix/snapcast/releases/download/v${SNAPCLIENT_VERSION}/snapclient_${SNAPCLIENT_VERSION}_armhf.deb
-dpkg -i snapclient_${SNAPCLIENT_VERSION}_armhf.deb
+sudo dpkg -i snapclient_${SNAPCLIENT_VERSION}_armhf.deb
 
 
 wget https://github.com/badaix/snapcast/releases/download/v${SNAPSERVER_VERSION}/snapsever_${SNAPSERVER_VERSION}_armhf.deb
-dpkg -i snapserver_${SNAPSERVER_VERSION}_armhf.deb
+sudo dpkg -i snapserver_${SNAPSERVER_VERSION}_armhf.deb
 
 rm snapclient_${SNAPCLIENT_VERSION}_armhf.deb
 rm snapserver_${SNAPSERVER_VERSION}_armhf.deb
